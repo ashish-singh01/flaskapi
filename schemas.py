5ad590a562315c4f79b9e15732262e637d6ede97
ashish <ashishjagjeet@gmail.com>
2023-07-10 17:11:24 +0530

    Adding tags for Store and Items

--- schemas.py
+++ schemas.py
@@ -1,17 +1,19 @@
 from marshmallow import Schema, fields
 
 class PlainItemSchemas(Schema):
-    id = fields.Str(dump_only = True)
+    id = fields.Int(dump_only = True)
     name = fields.Str(required = True)
     price = fields.Float(required = True)
     
 
 
 class PlainStoreSchemas(Schema):
-    id = fields.Str(dump_only = True)
+    id = fields.Int(dump_only = True)
     name = fields.Str(required = True)
     
-
+class PlainTagSchemas(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
 
 class ItemUpdateSchemas(Schema):
     name = fields.Str()
@@ -21,6 +23,19 @@ class ItemUpdateSchemas(Schema):
 class ItemSchemas(PlainItemSchemas):
     store_id = fields.Int(required = True)
     store = fields.Nested(PlainStoreSchemas(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchemas), dump_only=True)
 
 class StoreSchemas(PlainStoreSchemas):
     item = fields.List(fields.Nested(PlainItemSchemas()), dump_only= True)
+    tags = fields.List(fields.Nested(PlainTagSchemas()), dump_only=True)
+
+class TagSchemas(PlainTagSchemas):
+    store_id = fields.Int(load_only=True)
+    items = fields.List(fields.Nested(PlainItemSchemas()), dump_only=True)
+    store = fields.Nested(PlainStoreSchemas(), dump_only=True)
+
+
+class TagAndItemSchemas(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchemas)
+    tag = fields.Nested(TagSchemas)
